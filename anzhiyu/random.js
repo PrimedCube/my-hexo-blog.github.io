db7771503e9f65b7b07e272651eab3641dc8fab9
PrimedCube <a1138180962@163.com>
2025-03-22 12:41:41 +0800

    Site updated: 2025-03-22 12:41:41

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/22/hello-world/"];function toRandomPost(){
+var posts=["2025/03/22/hello-world/","2025/03/22/video-test/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
